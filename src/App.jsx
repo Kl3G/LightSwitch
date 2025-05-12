b7bd35cae0f1a777112a54d267d7208d08d30b90
Kl3G <qrapls159@naver.com>
2025-05-12 22:25:32 +0900

    added-comment

--- src/App.jsx
+++ src/App.jsx
@@ -16,9 +16,11 @@ function App() {
     <>
       <Background color={onOff} 
       onToggle={SwitchHendler} />
-      {/* "상태는 부모에서 관리하고, 자식 컴포넌트에는 props로 내려준다."
+      {/* 버튼을 Background.jsx로 옮기되, 
+      상태(state)는 App(부모)에서 계속 관리하기 위해서 자식 컴포넌트에 props로 내려준다.
       이걸 "상태 끌어올리기(Lifting State Up)" 라고 한다. */}
-      {/* 버튼을 Background.jsx로 옮기되, 상태는 App에서 계속 관리 */}
+      {/* SwitchHendler는 콜백함수다, 
+      콜백함수는 "호출할 수 있는 권한"과 호출할 함수의 "주소"를 전달한다. */}
     </>
   )
 }
